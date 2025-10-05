58c529f03befbd9e8bfe9ca00d5158bff4e71d1b
Aditha Doratiyawa <indegamedev@gmail.com>
2025-10-05 23:35:52 +1100

    Moved test bundles to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 extern crate core;
 
-use crate::factory::buildings::aggregator::Aggregator;
-use crate::factory::buildings::combiner::Combiner;
-use crate::factory::buildings::delinker::Delinker;
-use crate::factory::buildings::splitter::Splitter;
-use crate::factory::buildings::trunker::Trunker;
-use crate::factory::buildings::{SinkBuilding, SourceBuilding};
-use crate::factory::logical::{BasicDataType, DataAttribute, Dataset};
-use crate::grid::Direction;
 use crate::{
     assets::AssetPlugin,
     camera::GameCameraPlugin,
@@ -17,9 +9,8 @@ use crate::{
     grid::{Grid, GridPlugin, GridPosition},
     ui::UIPlugin,
 };
-use bevy::platform::collections::HashSet;
+use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
-use bevy::{math::I64Vec2, platform::collections::HashMap, prelude::*};
 
 mod assets;
 mod camera;
@@ -27,6 +18,7 @@ mod events;
 mod factions;
 mod factory;
 mod grid;
+mod test;
 mod ui;
 
 fn main() {
@@ -46,180 +38,10 @@ fn main() {
 }
 
 fn startup(mut commands: Commands) {
-    spawn_splitter_test(&mut commands);
-    spawn_delinker_test(&mut commands);
-    spawn_combiner_test(&mut commands);
-    spawn_trunking_test(&mut commands);
-}
-
-fn spawn_combiner_test(commands: &mut Commands) {
-    commands.spawn(SourceBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: -5, y: 1 }),
-        Direction::Right,
-        Dataset {
-            contents: HashMap::from([(
-                BasicDataType::Behavioural,
-                HashSet::<DataAttribute>::new(),
-            )]),
-        },
-        5.0,
-    ));
-    commands.spawn(SourceBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: -5, y: 2 }),
-        Direction::Right,
-        Dataset {
-            contents: HashMap::from([(BasicDataType::Biometric, HashSet::<DataAttribute>::new())]),
-        },
-        5.0,
-    ));
-    commands.spawn(Combiner::get_bundle(
-        GridPosition(I64Vec2 { x: -4, y: 1 }),
-        5.0,
-        Direction::Right,
-        2,
-    ));
-    commands.spawn(SinkBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: -3, y: 1 }),
-        Direction::Left,
-        None,
-    ));
-}
-fn spawn_trunking_test(commands: &mut Commands) {
-    commands.spawn(SourceBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: -5, y: 5 }),
-        Direction::Right,
-        Dataset {
-            contents: HashMap::from([(
-                BasicDataType::Behavioural,
-                HashSet::<DataAttribute>::new(),
-            )]),
-        },
-        100.0,
-    ));
-    commands.spawn(SourceBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: -5, y: 6 }),
-        Direction::Right,
-        Dataset {
-            contents: HashMap::from([(
-                BasicDataType::Behavioural,
-                HashSet::<DataAttribute>::new(),
-            )]),
-        },
-        5.0,
-    ));
-    commands.spawn(Trunker::get_bundle(
-        GridPosition(I64Vec2 { x: -4, y: 5 }),
-        10.0,
-        Direction::Right,
-        2,
-    ));
-    commands.spawn(SinkBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: -3, y: 5 }),
-        Direction::Left,
-        None,
-    ));
-}
-
-fn spawn_delinker_test(commands: &mut Commands) {
-    commands.spawn(SourceBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: 0, y: 1 + 5 }),
-        Direction::Right,
-        Dataset {
-            contents: HashMap::from([
-                (BasicDataType::Behavioural, HashSet::<DataAttribute>::new()),
-                (BasicDataType::Biometric, HashSet::<DataAttribute>::new()),
-            ]),
-        },
-        5.0,
-    ));
-    commands.spawn(Aggregator::get_bundle(
-        GridPosition(I64Vec2 { x: 1, y: 1 + 5 }),
-        1.0,
-        Direction::Right,
-    ));
-    // commands.spawn(PhysicalLink::get_spawn_bundle(GridPosition(I64Vec2 {
-    //     x: 1,
-    //     y: 1,
-    // })));
-    commands.spawn(PhysicalLink::get_bundle(GridPosition(I64Vec2 {
-        x: 2,
-        y: 1 + 5,
-    })));
-    commands.spawn(Delinker::get_bundle(
-        GridPosition(I64Vec2 { x: 3, y: 1 + 5 }),
-        50.,
-        Direction::Right, /* f32 */
-        /* grid::Direction */
-        2,
-    ));
-    commands.spawn(PhysicalLink::get_bundle(GridPosition(I64Vec2 {
-        x: 4,
-        y: 1 + 5,
-    })));
-    commands.spawn(PhysicalLink::get_bundle(GridPosition(I64Vec2 {
-        x: 4,
-        y: 2 + 5,
-    })));
-    commands.spawn(SinkBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: 5, y: 1 + 5 }),
-        Direction::Left,
-        None,
-    ));
-    commands.spawn(SinkBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: 5, y: 2 + 5 }),
-        Direction::Left,
-        None,
-    ));
-}
-
-fn spawn_splitter_test(commands: &mut Commands) {
-    commands.spawn(SourceBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: 0, y: 1 }),
-        Direction::Right,
-        Dataset {
-            contents: HashMap::from([(
-                BasicDataType::Behavioural,
-                HashSet::<DataAttribute>::new(),
-            )]),
-        },
-        5.0,
-    ));
-    commands.spawn(Aggregator::get_bundle(
-        GridPosition(I64Vec2 { x: 1, y: 1 }),
-        1.0,
-        Direction::Right,
-    ));
-    // commands.spawn(PhysicalLink::get_spawn_bundle(GridPosition(I64Vec2 {
-    //     x: 1,
-    //     y: 1,
-    // })));
-    commands.spawn(PhysicalLink::get_bundle(GridPosition(I64Vec2 {
-        x: 2,
-        y: 1,
-    })));
-    commands.spawn(Splitter::get_bundle(
-        GridPosition(I64Vec2 { x: 3, y: 1 }),
-        50.,
-        Direction::Right, /* f32 */ /* grid::Direction */
-    ));
-    commands.spawn(PhysicalLink::get_bundle(GridPosition(I64Vec2 {
-        x: 4,
-        y: 1,
-    })));
-    commands.spawn(PhysicalLink::get_bundle(GridPosition(I64Vec2 {
-        x: 4,
-        y: 2,
-    })));
-    commands.spawn(SinkBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: 5, y: 1 }),
-        Direction::Left,
-        None,
-    ));
-    commands.spawn(SinkBuilding::get_bundle(
-        GridPosition(I64Vec2 { x: 5, y: 2 }),
-        Direction::Left,
-        None,
-    ));
+    test::spawn_splitter_test(&mut commands);
+    test::spawn_delinker_test(&mut commands);
+    test::spawn_combiner_test(&mut commands);
+    test::spawn_trunking_test(&mut commands);
 }
 
 pub fn remove_physical_link_on_right_click(
